e47f06428b39eb75332fb97ef1833799493514d5
Pedro Duarte <pedro.duarte@gympass.com>
2019-08-13 13:58:54 -0300

    configure to open deeplink from web

--- src/App.js
+++ src/App.js
@@ -6,16 +6,38 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react';
+import { Linking, StyleSheet, Text, View } from 'react-native';
 
-import { Provider, connect } from "react-redux";
+import { Provider, connect } from 'react-redux';
 
-import Routes from "./Routes";
+import Routes from './Routes';
 
-import store from "./redux/store/configureStore";
+import store from './redux/store/configureStore';
 
 export default class App extends Component {
+  async componentDidMount() {
+    setTimeout(() => {
+      Linking.getInitialURL()
+        .then(url => {
+          console.log('URL is: ', url);
+          alert('URL is: ', url);
+        })
+        .catch(err => {
+          console.log('Err: ', err);
+        });
+    }, 3000);
+
+    // Linking.addEventListener("url", this.handleOpenURL);
+  }
+
+  componentWillUnmount() {
+    // Linking.removeEventListener("url", this.handleOpenURL);
+  }
+
+  handleOpenURL = event => {
+    alert(event.url);
+  };
 
   render() {
     return (
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,28 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, Linking } from "react-native";
+import { View, Text } from "react-native";
 
 export default class Home extends Component {
-  componentDidMount() {
-
-    Linking.getInitialURL()
-      .then((url) => {
-        console.log("URL is: ", url)
-      })
-      .catch(err => {
-        console.log("Err: ", err)
-      })
-
-    Linking.addEventListener("url", this.handleOpenURL);
-  }
-
-  componentWillUnmount() {
-    // Linking.removeEventListener("url", this.handleOpenURL);
-  }
-
-  handleOpenURL = event => {
-    alert(event.url);
-  };
-
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
